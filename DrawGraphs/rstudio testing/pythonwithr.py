1edd7ddec7d82f653adda275fb330e53ecf4883e
AStartledRaccoon <callum.cafferty@btinternet.com>
2019-08-20 15:45:04 +0100

    pythonwithr.py and rtesting.r now demonstrate regression

--- DrawGraphs/rstudio testing/pythonwithr.py
+++ DrawGraphs/rstudio testing/pythonwithr.py
@@ -1,1 +1,11 @@
 #python integration testing
+import subprocess
+import os
+import re
+path1=os.path.dirname(os.path.realpath(__file__)) #Gets the path for the current program
+csvin=input("Give a csv file: ")
+cmd=["Rscript","rtesting.r",str(csvin),path1] #makes a command to launch the r program, passes the user's path and the input
+x=subprocess.check_output(cmd, universal_newlines=True) #Sets x to the output of the command
+intercept=float((re.split("\n",x)[1]).strip()) #Isolates the numbers from the output
+grad=float((re.split("\n",x)[3]).strip())#And stores them in their relative variables
+
--- DrawGraphs/rstudio testing/rtesting.r
+++ DrawGraphs/rstudio testing/rtesting.r
@@ -1,2 +1,6 @@
 #space for testing stuff in r
-lm(dist~speed, data=cars)$coefficients
+pyin <- commandArgs(trailingOnly=TRUE) #gets the arguments that python passed in
+setwd(pyin[2]) #Sets the directory to the one passed in from the python program
+csvfile <- read.csv(file=pyin[1]) #Reads the csv file as listed by python
+formula1 <- as.formula(paste(as.name(colnames(csvfile)[1])," ~ ",as.name(colnames(csvfile)[2]))) #Makes the formula for linear regression on the csv file
+lm(formula1, data=csvfile)$coefficients #Returns the regression of that formula ($coefficients makes it so only the relevant bits are passed in)
