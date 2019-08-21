7f6551974836a927cfa01b7cf49d91de74e273bd
AStartledRaccoon <callum.cafferty@btinternet.com>
2019-08-21 15:22:35 +0100

    updated exponential code

--- DrawGraphs/rstudio testing/exponential_R.r
+++ DrawGraphs/rstudio testing/exponential_R.r
@@ -1,5 +1,4 @@
 setwd("~/Documents/EPQ/GitPath/DrawGraphs/rstudio testing")
-csvfile<-read.csv(file="testdata.csv",header=TRUE)
-
+csvfile<-read.csv(file="testdata.csv",header=TRUE
 expo<-nls(y~I(a*exp(b*x)),data=csvfile, start=list(a=1,b=1))
-expo
+#Fits y and x to the model of y=ae^bx
