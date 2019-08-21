06052fe988f2af866cb4219eef93ab930bc85535
Aliflo <tom@birkbeck.net>
2019-08-21 14:03:51 +0100

    yam

--- DrawGraphs/rstudio testing/linear_python.py
+++ DrawGraphs/rstudio testing/linear_python.py
@@ -11,6 +11,7 @@ cmd=["Rscript","linear_R.r",str(csvin),path1] #makes a command to launch the r p
 #lil batch script run from python
 x=subprocess.check_output(cmd, universal_newlines=True) #Sets x to the output of the command
 #which is the output of the final line of rtesting.r (the linear regression coefficients). universal_newlines forces it to work with linux and windows line endings
+print(x)
 intercept=float((re.split("\n",x)[1]).strip()) #Isolates the numbers from the output
 grad=float((re.split("\n",x)[3]).strip())#And stores them in their related variables
 
