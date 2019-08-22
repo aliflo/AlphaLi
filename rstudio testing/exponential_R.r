e10c3c41fb4bdfa3dcfce2d52a06f2b83c70f479
AStartledRaccoon <callum.cafferty@btinternet.com>
2019-08-22 15:25:09 +0100

    Removed a typo

--- rstudio testing/exponential_R.r
+++ rstudio testing/exponential_R.r
@@ -7,4 +7,4 @@ a_start<-as.numeric(summary(lm(col2~col1))$coefficients[2])
 b_start<-as.numeric(summary(lm(col2~col1))$coefficients[4]) #Creates the start points from linear regression coefficients of the logs
 f=as.formula(paste(as.name(colnames(csvfile)[2]),"~I(a*exp(b*",as.name(colnames(csvfile[1])),"))")) #Makes a formula that does exponential regression on the data
 expo<-nls(f,data=csvfile, start=list(a=exp(a_start),b=b_start)) #Does the exponential regression and stores the results
-expo #Outputs the resultsgg
+expo #Outputs the results
