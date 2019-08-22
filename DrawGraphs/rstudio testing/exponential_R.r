34b8f48f303426a1133460c4a2052e1fb286636f
AStartledRaccoon <callum.cafferty@btinternet.com>
2019-08-22 15:23:20 +0100

    Added comments

--- DrawGraphs/rstudio testing/exponential_R.r
+++ DrawGraphs/rstudio testing/exponential_R.r
@@ -1,10 +1,10 @@
-pyin <- commandArgs(trailingOnly=TRUE)
-setwd(pyin[2])
-csvfile <- read.csv(file=pyin[1])
+pyin <- commandArgs(trailingOnly=TRUE) #Gets inputs from python and stores them
+setwd(pyin[2]) #Sets the working directory to be the one recieved from python
+csvfile <- read.csv(file=pyin[1]) #Reads the csv that python instructed
 col1<-unlist(lapply(csvfile[colnames(csvfile)[1]], log))
-col2<-unlist(lapply(csvfile[colnames(csvfile)[2]], log))
+col2<-unlist(lapply(csvfile[colnames(csvfile)[2]], log)) #Takes logs of all the items in the first two columns and stores them in new columns
 a_start<-as.numeric(summary(lm(col2~col1))$coefficients[2])
-b_start<-as.numeric(summary(lm(col2~col1))$coefficients[4])
-f=as.formula(paste(as.name(colnames(csvfile)[2]),"~I(a*exp(b*",as.name(colnames(csvfile[1])),"))"))
-expo<-nls(f,data=csvfile, start=list(a=exp(a_start),b=b_start))
-expo
+b_start<-as.numeric(summary(lm(col2~col1))$coefficients[4]) #Creates the start points from linear regression coefficients of the logs
+f=as.formula(paste(as.name(colnames(csvfile)[2]),"~I(a*exp(b*",as.name(colnames(csvfile[1])),"))")) #Makes a formula that does exponential regression on the data
+expo<-nls(f,data=csvfile, start=list(a=exp(a_start),b=b_start)) #Does the exponential regression and stores the results
+expo #Outputs the results
