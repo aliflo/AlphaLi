5acb7b48d5d13a5013f47481522e3b84813c6460
Aliflo <tom@birkbeck.net>
2019-08-22 15:39:53 +0100

    added three csv files + work on splines_r
    work on splines r plus a few datasets to test wtih

--- DrawGraphs/rstudio testing/splines_R.r
+++ DrawGraphs/rstudio testing/splines_R.r
@@ -6,9 +6,12 @@
 library(SplinesUtils)
 pyin <- c("AIData.csv","C:/Users/tom/OneDrive/Documents/SWCHS/A-Levels/EPQ/DrawGraphs/DrawGraphs/rstudio testing")
 setwd <- pyin[2]#sets working directory to the above string
-csvfile <- read.csv(file=pyin[1],header=TRUE)#reads the csv file into a dataframe with headers
+csvfile <- read.csv(file="multiTimeline.csv",header=TRUE)#read.csv(file=pyin[1],header=TRUE)#reads the csv file into a dataframe with headers
 names(csvfile) <- c("months","searchInterest")#renames the headers becuase they're very long and cause formatting issues
-model <- lm(csvfile$searchInterest ~ bs(csvfile$months, df=5))#a linear model of months against a bspline of search interest
-piecewisePoly <- RegBsplineAsPiecePoly(model, "bs(csvfile$months, df = 5)",shift=FALSE)#creates the piecewise polynomials
+model <- lm(csvfile$searchInterest ~ bs(csvfile$months, df=7))#a linear model of months against a bspline of search interest
+piecewisePoly <- RegBsplineAsPiecePoly(model, "bs(csvfile$months, df = 7)",shift=FALSE)#creates the piecewise polynomials
 piecewisePoly
-piecewisePoly$PiecePoly$coef
+finalcoef <- piecewisePoly$PiecePoly$coef
+finalcoef[1, ] <- finalcoef[1, ] + model$coefficients[1]
+finalcoef
+attr(bs(csvfile$months, df=7),"knots")
