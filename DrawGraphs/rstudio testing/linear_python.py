569d62e23385ee5021735e22f0e4250f6be8263a
AStartledRaccoon <callum.cafferty@btinternet.com>
2019-08-22 13:27:35 +0100

    Slightly changed testdata.csv and also attempted to write code

--- DrawGraphs/rstudio testing/linear_python.py
+++ DrawGraphs/rstudio testing/linear_python.py
@@ -5,13 +5,11 @@ import subprocess
 import os
 import re
 path1=os.path.dirname(os.path.realpath(__file__)) #Gets the path for the current program
-print(path1)
 csvin=input("Give a csv file: ")
 cmd=["Rscript","linear_R.r",str(csvin),path1] #makes a command to launch the r program, passes the user's path and the input
 #lil batch script run from python
 x=subprocess.check_output(cmd, universal_newlines=True) #Sets x to the output of the command
 #which is the output of the final line of rtesting.r (the linear regression coefficients). universal_newlines forces it to work with linux and windows line endings
-print(x)
 intercept=float((re.split("\n",x)[1]).strip()) #Isolates the numbers from the output
 grad=float((re.split("\n",x)[3]).strip())#And stores them in their related variables
 
