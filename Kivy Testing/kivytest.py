c30706e0100f44278913921554eac04b8389cf69
AStartledRaccoon <callum.cafferty@btinternet.com>
2019-08-23 10:05:44 +0100

    Cleared kivytest.py

--- Kivy Testing/kivytest.py
+++ Kivy Testing/kivytest.py
@@ -1,12 +1,1 @@
 #experiments with kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from functools import partial
-class Kivy1(App):
-    def build(self):
-        return Button(text="Kivy!!")
-    def disable(self, instance, *args):
-        instance.disabled=True
-    def update(self, instance, *args)
-Kivy1().run()
