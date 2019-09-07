01b7524e26b7b72e2914e31db1ebd27c2afd8a95
Aliflo <tom@birkbeck.net>
2019-09-07 13:22:28 +0100

    Update IsolatedTesting.py

--- WIP Application/IsolatedTesting.py
+++ WIP Application/IsolatedTesting.py
@@ -1,17 +1,22 @@
-from tkinter import *
-root=Tk()
-elist=[]
-def up(event):
-	print (elist.index(event.widget))
-	if elist.index(event.widget)!=0:
-		elist[elist.index(event.widget)-1].focus_set()
-def down(event):
-	print (elist.index(event.widget))
-	if elist.index(event.widget)!=3:
-		elist[elist.index(event.widget)+1].focus_set()
-for i in range(4):
-	e=Entry(root)
-	e.grid(row=i)
-	e.bind("<Up>",up)
-	e.bind("<Down>",down)
-	elist.append(e)
+import os
+print(os.path.dirname(os.path.realpath(__file__)))
+
+
+
+##from tkinter import *
+##root=Tk()
+##elist=[]
+##def up(event):
+##	print (elist.index(event.widget))
+##	if elist.index(event.widget)!=0:
+##		elist[elist.index(event.widget)-1].focus_set()
+##def down(event):
+##	print (elist.index(event.widget))
+##	if elist.index(event.widget)!=3:
+##		elist[elist.index(event.widget)+1].focus_set()
+##for i in range(4):
+##	e=Entry(root)
+##	e.grid(row=i)
+##	e.bind("<Up>",up)
+##	e.bind("<Down>",down)
+##	elist.append(e)
