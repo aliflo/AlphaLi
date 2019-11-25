35ffe0ea5f53ed2391af0360937ecaf5241671d9
Aliflo <tom@birkbeck.net>
2019-11-25 19:47:26 +0000

    why are you reading commit messages dummy

--- WIP Application/MainProgram.py
+++ WIP Application/MainProgram.py
@@ -564,7 +564,7 @@ class CreateEquation():
 			x=subprocess.check_output(cmd, universal_newlines=True) #Sets x to the output of the command
 			#which is the output of the final line of rtesting.r (the linear regression coefficients). universal_newlines forces it to work with linux and windows line endings
 			intercept=((re.split("\n",x)[1]).strip()) #Isolates the numbers from the output
-			grad=((re.split("\n",x)[3]).strip())#And stores them in their related variables
+			grad=((re.split("\n",x)[3]).strip())#And stores them in their related variables 
 			self.__equations=str(grad)+"x+"+str(intercept)
 		if self.__method=="B-splines":
 			csvin=os.path.basename(datafilepath)
