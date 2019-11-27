3555efc3ed38b86d44557b5f4bf9125d9c419706
Aliflo <tom@birkbeck.net>
2019-11-27 13:19:40 +0000

    deleted everything LOL

--- Regression Programs/expontial_python.py
+++
@@ -1,9 +0,0 @@
-#exponential python
-import subprocess
-import os
-import re
-path1=os.path.dirname(os.path.realpath(__file__))
-csvin=input("Give a csv file: ")
-cmd=["Rscript","exponential_R.r",str(csvin),path1]
-x=subprocess.check_output(cmd, universal_newlines=True)
-print (x)
--- Regression Programs/splines_python.py
+++
@@ -1,1 +0,0 @@
-#see commenting for linear-python
